8548805cc747b9dd82c7dee82d50176a81be32ff
Saravanan <saravanan@ajirasoft.com>
2017-01-31 12:08:51 +0530

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        int startCount = Integer.parseInt(args[0]);
-        int endCount = Integer.parseInt(args[1]);
+        int startCount = 0;
+        int endCount = 1;
         for(int i = startCount ; i <= endCount; i++ ){
             if(i % 10 == 0)
                 Thread.sleep(250);
--- src/com/company/TestConsumer.java
+++ src/com/company/TestConsumer.java
@@ -3,7 +3,16 @@ package com.company;
 
 import com.rabbitmq.client.*;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.util.concurrent.TimeoutException;
 
 public class TestConsumer implements Runnable{
@@ -27,15 +36,18 @@ public class TestConsumer implements Runnable{
             e.printStackTrace();
         } catch (TimeoutException e) {
             e.printStackTrace();
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
-    public void startConsumer() throws IOException, TimeoutException, InterruptedException {
+    public void startConsumer() throws Exception {
+
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("52.54.13.102");
+        factory.setHost("gft-wnsqa.gofrugalconnect.com");
         factory.setPort(5672);
         factory.setVirtualHost("/");
         factory.setUsername("client");
-        factory.setPassword(":dwnd2$sck0@");
+        factory.setPassword("dwnd2$sck0");
         factory.setConnectionTimeout(20000);
         factory.setHandshakeTimeout(20000);
         factory.setShutdownTimeout(20000);
